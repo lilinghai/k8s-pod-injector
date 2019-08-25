a9a6720a6ca52f6a1df267d0bc6ce6f38d364e89
lilinghai <jidushanlixi@163.com>
2019-08-25 12:05:02 +0000

    webhook server

---
+++ webhook.go
@@ -0,0 +1,314 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/golang/glog"
+	"io/ioutil"
+	"k8s.io/api/admission/v1beta1"
+	admissionregistrationv1beta1 "k8s.io/api/admissionregistration/v1beta1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+	"k8s.io/kubernetes/pkg/apis/core/v1"
+	"net/http"
+	"strings"
+)
+
+var (
+	runtimeScheme = runtime.NewScheme()
+	codecs        = serializer.NewCodecFactory(runtimeScheme)
+	deserializer  = codecs.UniversalDeserializer()
+
+	// (https://github.com/kubernetes/kubernetes/issues/57982)
+	defaulter = runtime.ObjectDefaulter(runtimeScheme)
+)
+
+var ignoredNamespaces = []string {
+	metav1.NamespaceSystem,
+	metav1.NamespacePublic,
+}
+
+const (
+	admissionWebhookAnnotationInjectKey = "sidecar-injector-webhook.morven.me/inject"
+	admissionWebhookAnnotationStatusKey = "sidecar-injector-webhook.morven.me/status"
+)
+
+type WebhookServer struct {
+	server           *http.Server
+}
+
+// Webhook Server parameters
+type WhSvrParameters struct {
+	port int                 // webhook server port
+	certFile string          // path to the x509 certificate for https
+	keyFile string           // path to the x509 private key matching `CertFile`
+}
+
+type patchOperation struct {
+	Op    string      `json:"op"`
+	Path  string      `json:"path"`
+	Value interface{} `json:"value,omitempty"`
+}
+
+//从CRD获得sidecar模板
+type SidecarTemplate struct {
+	Items []struct {
+		Selector struct {
+			Injector string `json:"injector"`
+		} `json:"selector"`
+		Spec struct {
+			Containers  []corev1.Container  `json:"containers"`
+		} `json:"spec"`
+	} `json:"items"`
+}
+
+func init() {
+	_ = corev1.AddToScheme(runtimeScheme)
+	_ = admissionregistrationv1beta1.AddToScheme(runtimeScheme)
+	// defaulting with webhooks:
+	// https://github.com/kubernetes/kubernetes/issues/57982
+	_ = v1.AddToScheme(runtimeScheme)
+}
+
+// Check whether the target resoured need to be mutated
+func mutationRequired(ignoredList []string, metadata *metav1.ObjectMeta) bool {
+	// skip special kubernete system namespaces
+	for _, namespace := range ignoredList {
+		if metadata.Namespace == namespace {
+			glog.Infof("Skip mutation for %v for it' in special namespace:%v", metadata.Name, metadata.Namespace)
+			return false
+		}
+	}
+
+	annotations := metadata.GetAnnotations()
+	if annotations == nil {
+		annotations = map[string]string{}
+	}
+
+	status := annotations[admissionWebhookAnnotationStatusKey]
+	
+	// determine whether to perform mutation based on annotation for the target resource
+	var required bool
+	if strings.ToLower(status) == "injected" {
+		required = false;
+	} else {
+		switch strings.ToLower(annotations[admissionWebhookAnnotationInjectKey]) {
+		default:
+			required = false
+		case "y", "yes", "true", "on":
+			required = true
+		}
+	}
+	
+	glog.Infof("Mutation policy for %v/%v: status: %q required:%v", metadata.Namespace, metadata.Name, status, required)
+	return required
+}
+
+func addContainer(target, added []corev1.Container, basePath string) (patch []patchOperation) {
+	first := len(target) == 0
+	var value interface{}
+	for _, add := range added {
+		value = add
+		path := basePath
+		if first {
+			first = false
+			value = []corev1.Container{add}
+		} else {
+			path = path + "/-"
+		}
+		patch = append(patch, patchOperation {
+			Op:    "add",
+			Path:  path,
+			Value: value,
+		})
+	}
+	return patch
+}
+
+func updateAnnotation(target map[string]string, added map[string]string) (patch []patchOperation) {
+	for key, value := range added {
+		if target == nil || target[key] == "" {
+			target = map[string]string{}
+			patch = append(patch, patchOperation {
+				Op:   "add",
+				Path: "/metadata/annotations",
+				Value: map[string]string{
+					key: value,
+				},
+			})
+		} else {
+			patch = append(patch, patchOperation {
+				Op:    "replace",
+				Path:  "/metadata/annotations/" + key,
+				Value: value,
+			})
+		}
+	}
+	return patch
+}
+
+// create mutation patch for resoures
+func createPatch(pod *corev1.Pod, sidecars []corev1.Container, annotations map[string]string) ([]byte, error) {
+	var patch []patchOperation
+	
+	patch = append(patch, addContainer(pod.Spec.Containers, sidecars, "/spec/containers")...)
+	patch = append(patch, updateAnnotation(pod.Annotations, annotations)...)
+
+	return json.Marshal(patch)
+}
+
+func getSidecarFromCrd()(*SidecarTemplate,error){
+	resp,err:=http.Get("http://127.0.0.1:8001/apis/pods.injector.com/v1/namespaces/default/sidecars")
+	if err!=nil{
+		return nil,err
+	}
+	defer resp.Body.Close()
+
+	body,err := ioutil.ReadAll(resp.Body)
+	if err!=nil{
+		return nil,err
+	}
+	st:=&SidecarTemplate{}
+	if err=json.Unmarshal(body,st);err!=nil{
+		return nil,err
+	}
+	glog.Infof("crd resp is %s,sidecar template is %v",body,*st)
+	return st,nil
+}
+
+//校验pod.metadata的标签是否和sidecar匹配，筛选出符合条件的sidecar
+func filterSidecar(metadata *metav1.ObjectMeta,  st *SidecarTemplate)([]corev1.Container,error){
+	var res []corev1.Container
+	for i:=0;i<len(st.Items);i++{
+		labels := metadata.GetLabels()
+		if labels == nil {
+			labels = map[string]string{}
+		}
+		if label,ok:=labels["injector"];ok{
+			if label!=st.Items[i].Selector.Injector{
+				continue
+			}
+		}
+		res=append(res,st.Items[i].Spec.Containers...)
+	}
+	return res,nil
+}
+
+// main mutation process
+func (whsvr *WebhookServer) mutate(ar *v1beta1.AdmissionReview) *v1beta1.AdmissionResponse {
+	req := ar.Request
+	var pod corev1.Pod
+	if err := json.Unmarshal(req.Object.Raw, &pod); err != nil {
+		glog.Errorf("Could not unmarshal raw object: %v", err)
+		return &v1beta1.AdmissionResponse {
+			Result: &metav1.Status {
+				Message: err.Error(),
+			},
+		}
+	}
+
+	glog.Infof("AdmissionReview for Kind=%v, Namespace=%v Name=%v (%v) UID=%v patchOperation=%v UserInfo=%v",
+		req.Kind, req.Namespace, req.Name, pod.Name, req.UID, req.Operation, req.UserInfo)
+	
+	// determine whether to perform mutation
+	if !mutationRequired(ignoredNamespaces, &pod.ObjectMeta) {
+		glog.Infof("Skipping mutation for %s/%s due to policy check", pod.Namespace, pod.Name)
+		return &v1beta1.AdmissionResponse {
+			Allowed: true, 
+		}
+	}
+	st,err:=getSidecarFromCrd()
+	if err!=nil{
+		glog.Errorf("get crd sidecar template error %s",err)
+		return &v1beta1.AdmissionResponse {
+			Allowed: true,
+		}
+	}
+	sidecars,err:=filterSidecar(&pod.ObjectMeta,st)
+	if err!=nil || sidecars==nil{
+		glog.Infof("sidecars is nil")
+		return &v1beta1.AdmissionResponse {
+			Allowed: true,
+		}
+	}
+	glog.Infof("filtered sidecars %v",sidecars)
+	// Workaround: https://github.com/kubernetes/kubernetes/issues/57982
+	//applyDefaultsWorkaround(whsvr.sidecarConfig.Containers, whsvr.sidecarConfig.Volumes)
+	defaulter.Default(&corev1.Pod {Spec: corev1.PodSpec {Containers:sidecars}})
+	annotations := map[string]string{admissionWebhookAnnotationStatusKey: "injected"}
+	patchBytes, err := createPatch(&pod, sidecars, annotations)
+	if err != nil {
+		return &v1beta1.AdmissionResponse {
+			Result: &metav1.Status {
+				Message: err.Error(),
+			},
+		}
+	}
+	
+	glog.Infof("AdmissionResponse: patch=%v\n", string(patchBytes))
+	return &v1beta1.AdmissionResponse {
+		Allowed: true,
+		Patch:   patchBytes,
+		PatchType: func() *v1beta1.PatchType {
+			pt := v1beta1.PatchTypeJSONPatch
+			return &pt
+		}(),
+	}
+}
+
+// Serve method for webhook server
+func (whsvr *WebhookServer) serve(w http.ResponseWriter, r *http.Request) {
+	var body []byte
+	if r.Body != nil {
+		if data, err := ioutil.ReadAll(r.Body); err == nil {
+			body = data
+		}
+	}
+	if len(body) == 0 {
+		glog.Error("empty body")
+		http.Error(w, "empty body", http.StatusBadRequest)
+		return
+	}
+
+	// verify the content type is accurate
+	contentType := r.Header.Get("Content-Type")
+	if contentType != "application/json" {
+		glog.Errorf("Content-Type=%s, expect application/json", contentType)
+		http.Error(w, "invalid Content-Type, expect `application/json`", http.StatusUnsupportedMediaType)
+		return
+	}
+
+	var admissionResponse *v1beta1.AdmissionResponse
+	ar := v1beta1.AdmissionReview{}
+	if _, _, err := deserializer.Decode(body, nil, &ar); err != nil {
+		glog.Errorf("Can't decode body: %v", err)
+		admissionResponse = &v1beta1.AdmissionResponse {
+			Result: &metav1.Status {
+				Message: err.Error(),
+			},
+		}
+	} else {
+		admissionResponse = whsvr.mutate(&ar)
+	}
+
+	admissionReview := v1beta1.AdmissionReview{}
+	if admissionResponse != nil {
+		admissionReview.Response = admissionResponse
+		if ar.Request != nil {
+			admissionReview.Response.UID = ar.Request.UID
+		}
+	}
+
+	resp, err := json.Marshal(admissionReview)
+	if err != nil {
+		glog.Errorf("Can't encode response: %v", err)
+		http.Error(w, fmt.Sprintf("could not encode response: %v", err), http.StatusInternalServerError)
+	}
+	glog.Infof("Ready to write reponse ...")
+	if _, err := w.Write(resp); err != nil {
+		glog.Errorf("Can't write response: %v", err)
+		http.Error(w, fmt.Sprintf("could not write response: %v", err), http.StatusInternalServerError)
+	}
+}
